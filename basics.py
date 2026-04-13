00f567627397476c41b1d7eb56479fb559c3987c
Davy-odua <davyodua@gmail.com>
2026-04-13 06:22:18 +0300

    first commit

---
+++ basics.py
@@ -0,0 +1,30 @@
+#Variables, data types, type conversion
+
+#Data types
+#string, int, float, boolean
+integer = 1
+string = "one"
+boolean = True
+floats = 0.5
+
+print(f"{integer, string, boolean, floats}")
+print(string)
+
+#Variables
+name = "Davy"
+gpa = 9.0
+passed = True
+year = 3
+
+print(f"{name} is in year {year} and he a gpa of {gpa} which means that it is {passed} that he has passed ")
+
+#type conversion
+number = 123
+numb = float(123)
+print(numb)
+
+
+
+
+
+
